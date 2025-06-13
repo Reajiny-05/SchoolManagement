7f562eaa97dae29ad8d03d9318ec9bf1fe2995e7
Your-Full-Name <reajinyhin@gmail.com>
2025-06-13 22:52:08 +0700

    update with public, private, static

--- Student.java
+++ Student.java
@@ -8,13 +8,116 @@ public class Student {
     char gender;
     String email;
     String phoneNumber;
-    boolean task;
+    String task;
     Date endDate;
     String skillAndExperience;
     int taskPoint;
+    
+    public String fullName;
+    private String password;
+    private String studentID;
+    public String classAndYear;
+    private char gender;
+    private String email;
+    private String phoneNumber;
+    private String task;
+    public Date endDate;
+    private String skillAndExperience;
+    private int taskPoint;
+
+
+    // Getters & Setters 
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getClassAndYear() {
+        return classAndYear;
+    }
+
+    public void setClassAndYear(String classAndYear) {
+        this.classAndYear = classAndYear;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getSkillAndExperience() {
+        return skillAndExperience;
+    }
+
+    public void setSkillAndExperience(String skillAndExperience) {
+        this.skillAndExperience = skillAndExperience;
+    }
+
+    public int getTaskPoint() {
+        return taskPoint;
+    }
+
+    public void setTaskPoint(int taskPoint) {
+        this.taskPoint = taskPoint;
+    }
 
     public Student(String fullName, String password, String studentID, String classAndYear, char gender, String email,
-            String phoneNumber, boolean task, Date endDate, String skillAndExperience, int taskPoint) {
+            String phoneNumber, String task, Date endDate, String skillAndExperience, int taskPoint) {
         this.fullName = fullName;
         this.password = password;
         this.studentID = studentID;
@@ -28,4 +131,16 @@ public class Student {
         this.taskPoint = taskPoint;
     }
     
+    public static void isValidEmail() {
+    
+    }
+
+    private static void isValidPhoneNumber(){
+
+    }
+
 }
+
+
+
+
--- Teacher.java
+++ Teacher.java
@@ -5,21 +5,95 @@ public class Teacher {
     String email;
     String password;
     String phoneNumber;
-    String studentScheduleID;
     String studentPortfolioID;
+
+    public String fullName;
+    private String teacherID;
+    private String instituteName;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private studentPortfolioID;
+
+   //Getter and Setter
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getTeacherID() {
+        return teacherID;
+    }
+
+    public void setTeacherID(String teacherID) {
+        this.teacherID = teacherID;
+    }
+
+    public String getInstituteName() {
+        return instituteName;
+    }
+
+    public void setInstituteName(String instituteName) {
+        this.instituteName = instituteName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getStudentPortfolioID() {
+        return studentPortfolioID;
+    }
+
+    public void setStudentPortfolioID(String studentPortfolioID) {
+        this.studentPortfolioID = studentPortfolioID;
+    }
+
     
     public Teacher(String fullName, String teacherID, String instituteName, String email, String password,
-            String phoneNumber, String studentScheduleID, String studentPortfolioID) {
+            String phoneNumber, String studentPortfolioID) {
         this.fullName = fullName;
         this.teacherID = teacherID;
         this.instituteName = instituteName;
         this.email = email;
         this.password = password;
         this.phoneNumber = phoneNumber;
-        this.studentScheduleID = studentScheduleID;
+        
         this.studentPortfolioID = studentPortfolioID;
     }
 
+    
+    public static boolean isValidEmail() {
+        
+    }
+
+    public static boolean isValidPhoneNumber() {
+        
+    }
     
     
 }

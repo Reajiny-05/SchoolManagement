e56742da69afe7473344871e385b672af871bd6d
Your-Full-Name <reajinyhin@gmail.com>
2025-07-02 13:21:40 +0700

    update task

---
+++ object/Task.java
@@ -0,0 +1,12 @@
+import java.sql.Date;
+
+
+public class Task {
+    int taskID;
+    String taskName;
+    int workPoint;
+    String progress;
+    Date deadline;
+    Boolean importance_status;
+
+}

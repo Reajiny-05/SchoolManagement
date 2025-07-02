a1a1e9cf4b66da7d257f13a65cdd9a2da921b5e0
sreynich04 <sp6024010040@camtech.edu.kh>
2025-07-02 06:32:46 +0000

    Add constuctor to Task

--- object/Teacher1.java
+++ object/Teacher1.java
@@ -1,14 +1,20 @@
 public class Teacher1 {
-
     int teacherID;
     String email;
     String password;
     String username;
     String phoneNumber;
     String institude;
-
-    public Teacher1 {
-        
+    
+    public Teacher1(int teacherID, String email, String password, String username, String phoneNumber,
+            String institude) {
+        this.teacherID = teacherID;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.phoneNumber = phoneNumber;
+        this.institude = institude;
     }
-}
 
+    
+}

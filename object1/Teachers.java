3521bf78cc27da887c9b3286c6b72dbbf97cb23e
Your-Full-Name <reajinyhin@gmail.com>
2025-07-19 11:42:57 +0700

    Update teacher code

--- object1/Teachers.java
+++ object1/Teachers.java
@@ -1,13 +1,12 @@
 package object1;
 
 public class Teachers extends User{
-    private int score = 0;
 
     // Full constructor
     public Teachers(int userID, String email, String password, String username, String phoneNumber,
             String institude, int score) {
         super(userID, email, password, username, phoneNumber, institude);
-        this.score = score;
+    
     }
 
 
@@ -23,7 +22,19 @@ public class Teachers extends User{
 
     // Setter for score
     protected void setScore(int score) {
+    if (score < 0 || score > 10) {
+        System.out.println("Score must be between 0–10.");
+        this.score = 0;
+    } else {
         this.score = score;
     }
+}
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    
     
 }

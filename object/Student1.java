e60ee9a828da87ccccd2c8e45eac115e39b4f23a
Your-Full-Name <reajinyhin@gmail.com>
2025-07-19 11:10:15 +0700

    change

---
+++ object/Student1.java
@@ -0,0 +1,23 @@
+public class Student1 {
+
+    int studentID;
+    String email;
+    String password;
+    String username;
+    String phoneNumber;
+    int totalWorkPoint = 0;
+    String institude;
+    
+    public Student1(int studentID, String email, String password, String username, String phoneNumber,
+            int totalWorkPoint, String institude) {
+        this.studentID = studentID;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.phoneNumber = phoneNumber;
+        this.totalWorkPoint = totalWorkPoint;
+        this.institude = institude;
+    }
+
+}
+

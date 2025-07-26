588c9c8ca6dae9d4542ada0a1e65fadc9c5b1ce1
sreynich04 <sp6024010040@camtech.edu.kh>
2025-07-26 04:44:08 +0000

    change

--- object1/Teachers.java
+++ object1/Teachers.java
@@ -1,12 +1,19 @@
 package object1;
 
 public class Teachers extends User{
+    private String course = "";
 
-    // Full constructor
-    public Teachers(int userID, String email, String password, String username, String phoneNumber,
-            String institude) {
+    //full constructor
+    public Teachers(int userID, String email, String password, String username, String phoneNumber, String institude,
+            String course) {
         super(userID, email, password, username, phoneNumber, institude);
-    
+        this.course = course;
+    }
+
+    //constructor
+    public Teachers(String password, String username, String course) {
+        super(password, username);
+        this.course = course;
     }
 
     // Overloaded constructor
@@ -14,7 +21,20 @@ public class Teachers extends User{
         super(username, password);
     }
 
-     // Method to grade a student
+    //setter and getter
+    public String getcourse() {
+        return course;
+    }
+
+    protected void setcourse(String course) {
+        if (course != null && !course.trim().isEmpty()) {
+            this.course = course;
+        } else {
+            System.out.println("Invalid course name.");
+        }
+    }
+
+    // Method to grade a student
     public void gradeStudent(Students student, int score) {
         student.setTotalWorkPoint(score); // Or add to it if needed
         System.out.println("Graded student " + student.getUsername() + " with score " + score);
@@ -23,18 +43,17 @@ public class Teachers extends User{
     //
     @Override
     public String toString() {
-        return super.toString() + ", Teacher";
+        return super.toString() + ", Teacher [course = " + course + "]";
     }
 
-    //
     @Override
     public String displayUserInfo() {
-        return super.displayUserInfo() + " Teacher";
+        return super.displayUserInfo() + ", course = " + course;
     }
 
     @Override
     public boolean equals(Object obj) {
         return super.equals(obj); // using the equals method from User class to compare email, and reuse the logic from user class
     }
-    
+
 }

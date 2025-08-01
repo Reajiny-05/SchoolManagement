29cc96fec3ff009b7f18909ba81b23cb4e2627cc
Your-Full-Name <reajinyhin@gmail.com>
2025-08-01 13:09:03 +0700

    update code in Task

--- object1/Task.java
+++ object1/Task.java
@@ -13,19 +13,69 @@ public class Task {
     //contructor 
     public Task(int taskID, String taskName, int score, String progress, Date deadline) {
         this.taskID = taskID;
-        this.taskName = taskName;
-        this.score = score;
-        this.progress = progress;
-        this.deadline = deadline;
-        this.importance_status = importance_status;
+        setTaskName(taskName);
+        setScore(score);
+        setProgress(progress);
+        this.deadline = String.valueOf(deadline);
         taskCount++;
     }
 
-    //static method to return the number of tasks
+
+
+    private int getTaskID() {
+        return taskID;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    protected void setTaskName(String taskName){
+        if (isValidTaskName(taskName)) {
+            this.taskName = taskName;
+        }
+        else {
+            System.out.println("Task name cannot be empty.");
+        }
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    protected void setScore(int score) {
+        if (score >= 0 && score <= 10) {
+            this.score = score;
+        }
+        else {
+            System.outprintln("Score must be between 0 and 10.");
+        }
+    }
+
+    public String getProgress() {
+        return progress;
+    }
+
+    protected void setProgress(String progress) {
+        if (progress == null) {
+        System.out.println("Progress cannot be null.");
+        return;
+        }
+
+        progress = progress.trim().toLowerCase(); // normalize input
+
+        if (progress.equals("start") || progress.equals("in progress") || progress.equals("complete")) {
+            this.progress = progress;
+        } 
+        else {
+            System.out.println("Invalid progress status. Allowed values: start, in progress, complete.");
+        }
+    }
+
+     //static method to return the number of tasks
     public static int getTaskCount() {
     return taskCount;
     }
-
     //static method to make sure that there is a name for the task
     public static boolean isValidTaskName(String name) {
     return name != null && !name.trim().isEmpty();
@@ -38,6 +88,6 @@ public class Task {
                ", Progress=" + progress + ", Deadline=" + deadline + "]";
     
     }
-
+    
 
 }
